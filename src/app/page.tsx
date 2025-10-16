50160dc76ab57e9bcf94c42d8d2c0f07baa6682c
Devansh Verma <devansh@Devanshs-MacBook-Air.local>
2025-10-16 22:47:38 +0530

    03:trpc_setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { caller } from "@/trpc/server";
 
-export default function Home() {
+const Page = async () => {
+  const users = await caller.getUsers();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <h1>{JSON.stringify(users)}</h1>
       <Button>Click Me</Button>
     </div>
   );
-}
+};
+export default Page;
