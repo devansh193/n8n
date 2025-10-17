ade54fe39d9f1c5debaa96ecf97c3edcd6aa7305
Devansh Verma <devansh@Devanshs-MacBook-Air.local>
2025-10-18 00:46:39 +0530

    09:sidebar-layout

---
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -0,0 +1,12 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+  params: Promise<{ workflowId: string }>;
+}
+
+const Page = async ({ params }: PageProps) => {
+  await requireAuth();
+  const { workflowId } = await params;
+  return <div>Workflows Id: {workflowId}</div>;
+};
+export default Page;
