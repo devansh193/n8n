ca5d7e2127d0f75a4556920f0a1afb6b60b26c10
Devansh Verma <devansh@Devanshs-MacBook-Air.local>
2025-10-17 21:47:51 +0530

    06:background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import { requireAuth } from "@/lib/auth-utils";
-export const dynamic = "force-dynamic";
+"use client";
 
-const Page = async () => {
-  const session = await requireAuth();
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      <h1>{JSON.stringify(session)}</h1>
+      <h1>{JSON.stringify(data)}</h1>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   );
 };
