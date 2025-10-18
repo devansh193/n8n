e4a414120fe349a3b98fb07c2b262f8c09219e68
Devansh Verma <devansh@Devanshs-MacBook-Air.local>
2025-10-18 21:12:54 +0530

    10_payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/db";
+import { polarClient } from "./polar";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -10,4 +12,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "db8c6dc4-7454-4e84-9efc-a4bef6f5e85e",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
